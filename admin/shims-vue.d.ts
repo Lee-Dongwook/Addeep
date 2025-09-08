b1739b63d88c64a1e62cfa839b322a9d59e39aba
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-09-08 18:55:27 +0900

    fix: refactor admin

--- admin/shims-vue.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-empty-object-type */
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
