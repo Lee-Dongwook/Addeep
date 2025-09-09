3a4dd85872faf74d6d40433b44aa5d38815d1dc6
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-09-09 21:32:41 +0900

    feat: add image for banner of Addeep GPR

--- src/app/(landing)/addeep-is/summary/page.tsx
+++ src/app/(landing)/addeep-is/summary/page.tsx
@@ -6,6 +6,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useSearchParams } from "next/navigation";
 import { useResponsive } from "../../../../lib/useResponsive";
+import { NEXT_PUBLIC_CDN_BASE } from "../../../../lib/env";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -143,18 +144,40 @@ function SummaryAI() {
   }
 
   return (
-    <section className="px-2 py-4 mb-20 flex flex-col flex-1">
+    <section className="mb-20 flex flex-col flex-1">
+      <div className="w-full text-center">
+        <div
+          className="w-full h-[600px] rounded-lg flex flex-col items-center justify-center"
+          style={{
+            background: `url(${NEXT_PUBLIC_CDN_BASE}/images/AISummaryBanner.png)`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            border: "1px solid #E5E7EB",
+          }}
+        >
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white mb-4">
+            Addeep GPR
+          </h1>
+          <h3 className="text-2xl font-bold text-white mb-4">
+            Introducing a non-conversational,
+          </h3>
+          <h3 className="text-2xl font-bold text-white mb-4">
+            fully automated generative AI that understands your mindset.
+          </h3>
+          <button
+            className="mt-4 mb-6 px-6 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition-colors duration-200 font-medium"
+            onClick={() => router.push("/addeep-is/summary/gpr")}
+          >
+            Explore Technology
+          </button>
+        </div>
+      </div>
       <div className="flex flex-col flex-1 p-28">
         <h2 className="mb-4 text-[56px] font-bold">
           Redefining <span className="text-[#B641E9]">Web 3.0 </span>
           Social Media
         </h2>
-        {/* <button
-          className="mt-4 mb-6 px-6 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition-colors duration-200 font-medium"
-          onClick={() => router.push("/addeep-is/summary/gpr")}
-        >
-          내용 더보기
-        </button> */}
         <div className="border border-[#B641E9] text-[#B641E9] w-20 h-px mb-4"></div>
         <p className="text-2xl leading-relaxed">
           <span className="text-[#B641E9] font-bold">Addeep-GPR-1</span>{" "}
