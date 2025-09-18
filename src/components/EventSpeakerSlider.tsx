075f29bd8c059506b8494ce48b188d3559d2db61
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-09-18 17:32:57 +0900

    feat: navigation custom

--- src/components/EventSpeakerSlider.tsx
+++ src/components/EventSpeakerSlider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay } from "swiper/modules";
 import { NEXT_PUBLIC_CDN_BASE } from "../lib/env";
@@ -14,7 +15,9 @@ export default function EventSpeakerSlider({
   onClick: (e: any) => void;
   uuid: number;
 }) {
-  console.log(uuid, typeof uuid);
+  const prevRef = useRef<HTMLButtonElement | null>(null);
+  const nextRef = useRef<HTMLButtonElement | null>(null);
+
   const speakers =
     uuid === 1
       ? [
@@ -40,7 +43,16 @@ export default function EventSpeakerSlider({
         modules={[Navigation, Autoplay]}
         spaceBetween={30}
         slidesPerView={1}
-        navigation
+        navigation={{
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
+        onBeforeInit={(swiper) => {
+          // @ts-ignore
+          swiper.params.navigation.prevEl = prevRef.current;
+          // @ts-ignore
+          swiper.params.navigation.nextEl = nextRef.current;
+        }}
         autoplay={{ delay: 10000 }}
         loop
         className="h-full rounded-l-2xl"
@@ -49,6 +61,19 @@ export default function EventSpeakerSlider({
           <SwiperSlide key={idx}>
             <div className="flex flex-col items-center justify-center bg-white aspect-square w-full h-[800px]">
               <img src={sp.img} className="w-full h-full object-cover" />
+              {/* 커스텀 버튼 */}
+              <button
+                ref={prevRef}
+                className="absolute left-2 top-1/2 -translate-y-1/2 z-10 bg-black/50 text-white p-2 rounded-full opacity-100"
+              >
+                <h4 className="text-2xl">◀</h4>
+              </button>
+              <button
+                ref={nextRef}
+                className="absolute left-[470px] top-1/2 -translate-y-1/2 z-10 bg-black/50 text-white p-2 rounded-full opacity-100"
+              >
+                <h4 className="text-2xl">▶</h4>
+              </button>
             </div>
           </SwiperSlide>
         ))}
