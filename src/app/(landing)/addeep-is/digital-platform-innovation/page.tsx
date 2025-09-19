764135a8457e1a1cd67d8c1c04c86147db877be6
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-09-19 12:35:12 +0900

    fix: digital innovation

--- src/app/(landing)/addeep-is/digital-platform-innovation/page.tsx
+++ src/app/(landing)/addeep-is/digital-platform-innovation/page.tsx
@@ -9,28 +9,28 @@ const DigitalPlatformInnovation = () => {
   if (isMobile || isTablet) {
     return (
       <div className="stage relative mb-20">
-        <div className="mx-auto grid max-w-6xl grid-cols-1 gap-8 px-6 py-24 md:grid-cols-2 md:py-32">
+        <div className="mx-auto w-full p-6">
           <div className="space-y-20">
             <section className="copy">
               <h2 className="mb-4 text-2xl font-montserrat font-semibold text-pink-500">
                 Addeep is
               </h2>
-              <h2 className="mb-4 text-2xl font-semibold text-pink-500">
+              <h2 className="mb-10 text-2xl font-semibold text-pink-500">
                 GPR Innovation
               </h2>
               <YoutubePlayer videoId="xUG4jmCCZWU" />
-              <p className="text-xl leading-relaxed mt-8">
+              <p className="text-lg md:text-xl leading-loose md:leading-loose mt-12">
                 we aspire to create an integrated platform that smartly connects
                 the mutual needs of all members within the social media platform
                 and media content industry, within the digital platform
                 ecosystem. We innovate in a user-centeric market to make the
                 value of human connections even more enjoyable.
               </p>
-              <p className="text-xl leading-relaxed mt-8">
+              <p className="text-lg md:text-xl leading-loose md:leading-loose mt-8">
                 We efficiently accumulate vast amounts of structured and
                 unstructured data by using big data and AI technologies.
               </p>
-              <p className="text-xl leading-relaxed mt-8">
+              <p className="text-lg md:text-xl leading-loose md:leading-loose mt-8">
                 Through multidimensional analysis, reinterpretation, and
                 reprocessing, we effectively connect and reallocate all human
                 and material resources surrounding the content market.
