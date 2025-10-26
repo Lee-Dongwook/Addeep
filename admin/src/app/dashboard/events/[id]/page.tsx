d6691902ee0cd01aebfd786cc50c74654cde9ebd
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-10-26 16:34:51 +0900

    feat: event detail page inquiry

--- admin/src/app/dashboard/events/[id]/page.tsx
+++ admin/src/app/dashboard/events/[id]/page.tsx
@@ -1,3 +1,51 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { supabase } from "../../../../../lib/supabase";
+
 export default function EventDetailPage() {
-  return <div>Hello</div>;
+  const params = useParams();
+  const id = params.id as unknown as number;
+
+  const getEventDetailData = async () => {
+    try {
+      const { data, error } = await supabase
+        .from("events")
+        .select("*")
+        .eq("id", id);
+
+      if (error) {
+        console.error("Supabase 에러:", error);
+        throw error;
+      }
+      return data;
+    } catch (err) {
+      console.error(`getEventDetailData 에러:`, err);
+      throw err;
+    }
+  };
+
+  const {
+    data: eventDetail,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["eventDetail", id],
+    queryFn: () => getEventDetailData(),
+    retry: 0,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    staleTime: 30000,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return <div>{eventDetail?.[0].title}</div>;
 }
