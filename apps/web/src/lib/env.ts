c902963b514201f637231230492c00a759430d97
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-11-23 21:59:30 +0900

    hotfix: url

--- apps/web/src/lib/env.ts
+++ apps/web/src/lib/env.ts
@@ -1,2 +1,2 @@
 export const NEXT_PUBLIC_CDN_BASE =
-  "https://wkohurcxhbfhxbouyxue.supabase.co/storage/v1/object/public/assets-addeep";
+  "https://wkohurcxhbfhxbouyxue.supabase.co/storage/v1/object/public/assets-addeep/images";
